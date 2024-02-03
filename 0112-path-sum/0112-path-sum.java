e6162bc6017d343d6a5e173c94ae9e0da6aabb64
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-02-03 16:51:09 -0700

    Time: 2 ms (7.40%), Space: 43.5 MB (18.60%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -13,22 +13,60 @@
  *     }
  * }
  */
-class Solution {
-    int target;
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        target = targetSum;
-        return dfs(root, 0); 
-    }
+// class Solution {
+//     int target;
+//     public boolean hasPathSum(TreeNode root, int targetSum) {
+//         target = targetSum;
+//         return dfs(root, 0); 
+//     }
     
-    public boolean dfs(TreeNode root, int curr){
+//     public boolean dfs(TreeNode root, int curr){
+//         if(root == null)
+//             return false;
+//         if(root.left == null && root.right == null)
+//             return root.val + curr == target;
+//         curr = curr + root.val;
+//         boolean left = dfs(root.left, curr);
+//         boolean right = dfs(root.right, curr);
+        
+//         return left || right;
+//     }
+// }
+
+
+class Pair{
+    TreeNode node;
+    int curr;
+    Pair(TreeNode node, int curr){
+        this.node = node;
+        this.curr = curr;
+    }
+}
+
+class Solution{
+    public boolean hasPathSum(TreeNode root, int targetSum){
         if(root == null)
             return false;
-        if(root.left == null && root.right == null)
-            return root.val + curr == target;
-        curr = curr + root.val;
-        boolean left = dfs(root.left, curr);
-        boolean right = dfs(root.right, curr);
+        Stack<Pair> stack = new Stack<>();
+        stack.push(new Pair(root, 0));
         
-        return left || right;
+        
+        
+        while(!stack.isEmpty()){
+            Pair pop = stack.pop();
+            TreeNode node = pop.node;
+            if(node.left == null && node.right == null)
+                if(pop.curr + node.val == targetSum)
+                        return true;
+            int curr = pop.curr;
+            curr = curr + node.val;
+            if(node.left != null)
+                stack.push(new Pair(node.left, curr));
+            if(node.right != null)
+                stack.push(new Pair(node.right, curr));
+        }        
+        
+        return false;
+        
     }
 }
