181563e72fd0545250aa41e7c2b96d9469b4e4be
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-02-04 10:22:11 -0700

    Time: 15 ms (6.50%), Space: 53 MB (14.34%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -13,19 +13,49 @@
  *     }
  * }
  */
-class Solution {
-    public int goodNodes(TreeNode root) {
-        return dfs(root, Integer.MIN_VALUE);
+// class Solution {
+//     public int goodNodes(TreeNode root) {
+//         return dfs(root, Integer.MIN_VALUE);
         
+//     }
+//     public int dfs(TreeNode node, int maxSoFar){
+//         if(node == null)
+//             return 0;
+//         int left = dfs(node.left, Math.max(maxSoFar,  node.val));
+//         int right = dfs(node.right, Math.max(maxSoFar, node.val));
+//         int ans = left + right;
+//         if(node.val >= maxSoFar)
+//             ans = ans + 1;
+//         return ans;
+//     }
+// }
+
+class Pair{
+    TreeNode node;
+    int maxSoFar;
+    Pair(TreeNode node, int maxSoFar){
+        this.node = node;
+        this.maxSoFar = maxSoFar;
     }
-    public int dfs(TreeNode node, int maxSoFar){
-        if(node == null)
+}
+class Solution{
+    public int goodNodes(TreeNode root){
+        if(root == null)
             return 0;
-        int left = dfs(node.left, Math.max(maxSoFar,  node.val));
-        int right = dfs(node.right, Math.max(maxSoFar, node.val));
-        int ans = left + right;
-        if(node.val >= maxSoFar)
-            ans = ans + 1;
+        Stack<Pair> stack = new Stack<>();
+        int ans = 0;
+        stack.push(new Pair(root, Integer.MIN_VALUE));
+        while(! stack.isEmpty()){
+            Pair pair = stack.pop();
+            TreeNode node = pair.node;
+            int maxSoFar = pair.maxSoFar;
+            if(node.val >= maxSoFar)
+                ans++;
+            if(node.left != null)
+                stack.push(new Pair(node.left, Math.max(maxSoFar, node.val)));
+            if(node.right != null)
+                stack.push(new Pair(node.right, Math.max(maxSoFar, node.val)));
+        }
         return ans;
     }
 }
