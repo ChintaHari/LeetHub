f164eaeb23c3db153b05be3fae080ab3ce9c2ebd
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-02-09 09:04:40 -0700

    Time: 9 ms (35.72%), Space: 45.9 MB (32.44%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,26 +1,37 @@
 class Solution {
+    boolean[][] visited;
+    Queue<int[]> queue = new LinkedList<>();
+    int rows=0, cols=0;
+    int[][] dirs = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
     public int nearestExit(char[][] maze, int[] entrance) {
-        int rows = maze.length;
-        int cols = maze[0].length;
-        int[][] dirs = new int[][]{{1,0}, {-1,0}, {0,1}, {0,-1}};
-        Queue<int[]> queue= new LinkedList<>();
+        rows = maze.length;
+        cols = maze[0].length;
+        visited = new boolean[rows][cols];
         int startRow = entrance[0], startCol = entrance[1];
-        maze[startRow][startCol] = '+';
-        queue.add(new int[]{startRow, startCol, 0});
-        while(! queue.isEmpty()){
-            int[] remove = queue.remove();
-            int currRow = remove[0], currCol = remove[1], currDistance = remove[2];
-            for(int[] dir : dirs){
-                int nextRow = currRow + dir[0];
-                int nextCol = currCol + dir[1];
-                if(nextRow >=0 && nextRow < rows && nextCol >=0 && nextCol < cols && maze[nextRow][nextCol]=='.'){
-                    if(nextRow ==0 || nextRow == rows-1 || nextCol ==0 || nextCol == cols-1)
-                        return currDistance + 1;
-                    maze[nextRow][nextCol] = '+';
-                    queue.add(new int[]{nextRow, nextCol, currDistance + 1});
+        visited[startRow][startCol] = true;
+        queue.offer(new int[]{startRow, startCol, 0});
+        
+        while(!queue.isEmpty()){
+            int[] state = queue.poll();
+            int currRow = state[0], currCol = state[1], steps = state[2];
+           
+            for(int[] dir: dirs){
+                int nextRow = currRow + dir[0], nextCol = currCol + dir[1];
+                if(valid(nextRow, nextCol, maze)){
+                     if(nextRow == 0 || nextRow == rows - 1 || nextCol ==0 || nextCol == cols-1)
+                            return steps+1;
+                    visited[nextRow][nextCol] = true;
+                    queue.offer(new int[]{nextRow, nextCol, steps+1});
                 }
             }
         }
         return -1;
+       
+    }
+    
+    public boolean valid(int nextRow, int nextCol, char[][] maze){
+        return nextRow >=0 && nextRow < rows && nextCol >= 0 
+            && nextCol < cols && !visited[nextRow][nextCol]
+            && maze[nextRow][nextCol] == '.';
     }
 }
