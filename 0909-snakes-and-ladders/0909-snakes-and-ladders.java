b29fd8489dccf44d843fdfab05c55fa14f9d28a0
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-02-09 12:08:08 -0700

    Time: 7 ms (62.88%), Space: 45 MB (26.61%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -1,54 +1,41 @@
-
-
 class Solution {
     public int snakesAndLadders(int[][] board) {
-        int n = board.length;
-        int target = n * n;
-
-        Queue<Integer> queue = new LinkedList<>();
+        int n = board.length, target = n*n; 
         boolean[] visited = new boolean[target + 1];
+        Queue<Integer> queue = new LinkedList<>();
         queue.offer(1);
         visited[1] = true;
         int moves = 0;
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int curr = queue.poll();
-
-                if (curr == target) {
+        
+        while(!queue.isEmpty()){
+            int squareAtCurrentLevel = queue.size();
+            for(int i=0; i<squareAtCurrentLevel; i++){
+                int currSquare = queue.poll();
+                if(currSquare == target)
                     return moves;
-                }
-
-                for (int next = curr + 1; next <= Math.min(curr + 6, target); next++) {
-                    int[] coords = getCoordinates(next, n);
-                    int row = coords[0];
-                    int col = coords[1];
-
+                for(int next = currSquare + 1; next <= Math.min(currSquare+6, target); next++){
+                    int[] position = getPosition(next, n);
+                    int row = position[0], col = position[1];
                     int destination = board[row][col] == -1 ? next : board[row][col];
-
-                    if (!visited[destination]) {
+                    
+                    if(! visited[destination]){
                         visited[destination] = true;
                         queue.offer(destination);
                     }
                 }
             }
-            moves++;
+            moves ++;
         }
-
-        return -1; 
+        return -1;
     }
+    
+     private int[] getPosition(int next, int n) {
+        int row = (next - 1) / n,col = (next - 1) % n;
 
-    private int[] getCoordinates(int num, int n) {
-        int row = (num - 1) / n;
-        int col = (num - 1) % n;
-
-        if (row % 2 == 1) {
+        if (row % 2 == 1) 
             col = n - 1 - col;
-        }
-
+    
         row = n - 1 - row;
-
         return new int[]{row, col};
     }
 }
