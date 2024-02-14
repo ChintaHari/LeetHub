f90e4039874b942866892b198b204c37bac5dd3d
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-02-14 14:59:28 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (78.71%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,36 +14,19 @@
  * }
  */
 class Solution {
-    List<Integer> tree1 = new ArrayList<>();
-    List<Integer> tree2 = new ArrayList<>();
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // if(p==null && q==null)
-        //     return true;
-        // if((p==null && q!=null) || (q==null && p!=null))
-        //     return false;
-        // if(p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
-        //     return true;
-        // return false;
+     
         
         if(p == null && q == null)
             return true;
         if((p==null && q!=null) || (p!=null && q==null))
             return false;
-        dfs(p, tree1);
-        dfs(q, tree2);
         
-        return tree1.equals(tree2);
-        
+        if(p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
+            return true;
+        return false;
     }
     
-    public void dfs(TreeNode node, List<Integer> tree){
-        if(node == null){
-            tree.add(null);
-            return;
-        }
-           
-        tree.add(node.val);
-        dfs(node.left, tree);
-        dfs(node.right, tree);
-    }
+  
 }
