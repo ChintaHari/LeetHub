b349d17d02a01557ff50c305514157cbdfb57f5d
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-02-14 14:48:47 -0700

    Time: 91 ms (61.33%), Space: 48 MB (29.37%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,55 @@
+class State{
+    String node;
+    int words;
+    State(String node, int words){
+        this.node = node;
+        this.words = words;
+    }
+}
+class Solution {
+    Queue<State> queue = new LinkedList<>();
+    Set<String> visited = new HashSet<>();
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        
+        if (!wordSet.contains(endWord))
+            return 0;
+
+        queue.offer(new State(beginWord, 1));
+        visited.add(beginWord);
+
+        while (!queue.isEmpty()) {
+            State state = queue.poll();
+            String node = state.node;
+            int words = state.words;
+
+            if (node.equals(endWord))
+                return words;
+
+            for (String neighbour : calculateNeighbours(node, wordSet)) {
+                if (!visited.contains(neighbour)) {
+                    visited.add(neighbour);
+                    queue.offer(new State(neighbour, words + 1));
+                }
+            }
+        }
+        return 0;
+    }
+
+    public List<String> calculateNeighbours(String node, Set<String> wordSet) {
+        List<String> list = new ArrayList<>();
+        char[] chars = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        for (int i = 0; i < node.length(); i++) {
+            char[] charArray = node.toCharArray();
+            for (char ch : chars) {
+                charArray[i] = ch;
+                String modifiedWord = new String(charArray);
+                if (wordSet.contains(modifiedWord))
+                    list.add(modifiedWord);
+            }
+        }
+        return list;
+    }
+}
