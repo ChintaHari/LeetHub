70f3a7a292c9b1df8de5d2a7175a3e10187525a4
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-02-18 09:28:53 -0700

    Time: 221 ms (11.25%), Space: 59.5 MB (67.01%) - LeetHub

--- 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.java
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.java
@@ -1,35 +1,17 @@
 class Solution {
     public int halveArray(int[] nums) {
-        // PriorityQueue<Double> heap = new PriorityQueue<>(Comparator.reverseOrder());
-        // for(int ele: nums)
-        //     heap.add((double) ele);
-        // double sum = heap.stream().mapToDouble(Double::doubleValue).sum();
-        // double currSum = sum;
-        // int ans=0;
-        // while(currSum > sum/2){
-        //     double maxElement = heap.remove();
-        //     heap.add(maxElement/2);
-        //     currSum = currSum - maxElement/2;
-        //     ans++;
-        // }
-        // return ans;
-        double target = 0;
         PriorityQueue<Double> heap = new PriorityQueue<>(Comparator.reverseOrder());
-        
-        for (double num: nums) {
-            target += num;
-            heap.add(num);
-        }
-        
-        target /= 2;
-        int ans = 0;
-        while (target > 0) {
+        for(int ele: nums)
+            heap.add((double) ele);
+        double sum = heap.stream().mapToDouble(Double::doubleValue).sum();
+        double currSum = sum;
+        int ans=0;
+        while(currSum > sum/2){
+            double maxElement = heap.remove();
+            heap.add(maxElement/2);
+            currSum = currSum - maxElement/2;
             ans++;
-            double x = heap.remove();
-            target -= (x / 2);
-            heap.add(x / 2);
         }
-        
         return ans;
     }
 }
