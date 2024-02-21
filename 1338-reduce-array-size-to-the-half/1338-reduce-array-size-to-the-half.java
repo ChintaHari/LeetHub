92ae37f6690b2b07affba4afbac5f5c138d9f90f
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-02-21 15:46:02 -0700

    Time: 53 ms (17.55%), Space: 65.6 MB (5.19%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = arr.length;
+        for(int ele : arr)
+            map.put(ele, map.getOrDefault(ele,0)+1);
+            
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+        for(int key: map.keySet())
+            maxHeap.add(key);
+        
+        int currNoOfElements = n;
+        int minSet = 0;
+        while(currNoOfElements > n/2){
+            int pop = maxHeap.remove();
+            currNoOfElements = currNoOfElements - map.get(pop);
+            map.remove(pop);
+            minSet++;
+        }
+        return minSet;
+        
+    }
+}
