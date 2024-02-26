4b13f1f68b9db46d397b4fa9e516e4a471ad377d
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-02-26 16:29:35 -0700

    Time: 4 ms (11.89%), Space: 40.5 MB (99.70%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        long left = Arrays.stream(nums).max().getAsInt();
+        long right = Arrays.stream(nums).sum();
+        while(left < right){
+            long mid = left + (right - left)/2;
+            
+            int subArrays = 1; 
+            long currSum = 0;
+            for(int num: nums){
+                currSum += num;
+                if(currSum > mid){ 
+                    subArrays++;
+                    currSum = num; 
+                }
+            }
+            
+            if(subArrays <= k) 
+                right = mid;
+            else 
+                left = mid + 1;
+        }
+        return (int)left;
+    }
+}
