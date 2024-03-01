779d9831bc92f2b709472c38cfd54cf868bdc874
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-01 12:27:10 -0700

    Time: 0 ms (100.00%), Space: 41.8 MB (89.56%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,37 @@
+class Solution {
+    HashMap<Character, String> hm = new HashMap<>();
+    List<String> result = new ArrayList<>();
+    StringBuilder curr = new StringBuilder();
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) 
+            return new ArrayList<>();
+        
+        hm.put('2', "abc");
+        hm.put('3', "def");
+        hm.put('4', "ghi");
+        hm.put('5', "jkl");
+        hm.put('6', "mno");
+        hm.put('7', "pqrs");
+        hm.put('8', "tuv");
+        hm.put('9', "wxyz");
+        
+        backtrack(0, digits);
+        
+        return result;
+    }
+
+    public void backtrack(int index, String digits) {
+        if (curr.length() == digits.length()) {
+            result.add(curr.toString());
+            return;
+        }
+
+        String possibleLetters = hm.get(digits.charAt(index));
+        for (char letter : possibleLetters.toCharArray()) {
+            curr.append(letter);
+            backtrack(index + 1, digits);
+            curr.deleteCharAt(curr.length() - 1); 
+        }
+    }
+}
