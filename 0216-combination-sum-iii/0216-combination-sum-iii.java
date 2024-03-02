9918d5ee8c14edf5a61cabd12d36465d45c04751
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-02 10:09:39 -0700

    Time: 0 ms (100.00%), Space: 41.1 MB (22.84%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,27 +1,55 @@
+// class Solution {
+//     //List<List<Integer>> result = new ArrayList<>();
+//     Set<Set<Integer>> result = new HashSet<>();
+//     List<Integer> curr = new ArrayList<>();
+//     public List<List<Integer>> combinationSum3(int k, int n) {
+//         for(int i=1; i<=9; i++)
+//             backtrack(i,k,n,0);
+//         return result.stream()
+//         .map(ArrayList::new)
+//         .collect(Collectors.toList());
+//     }
+    
+//     public void backtrack(int i, int k, int n, int currSum){
+//         if(curr.size() == k && currSum == n){
+//             result.add(new HashSet<>(curr));
+//             return;
+//         }
+        
+//         for(int j=i; j<=9; j++){
+//             if(currSum + j <= n){
+//                 curr.add(j);
+//                 backtrack(j+1, k, n, currSum + j);
+//                 curr.remove(curr.size() - 1);
+//             }
+//         }
+//     }
+// }
+
+
+
 class Solution {
-    //List<List<Integer>> result = new ArrayList<>();
-    Set<Set<Integer>> result = new HashSet<>();
+    List<List<Integer>> result = new ArrayList<>();
     List<Integer> curr = new ArrayList<>();
+
     public List<List<Integer>> combinationSum3(int k, int n) {
-        for(int i=1; i<=9; i++)
-            backtrack(i,k,n,0);
-        return result.stream()
-        .map(ArrayList::new)
-        .collect(Collectors.toList());
+        backtrack(1, k, n, 0);
+        return result;
     }
     
-    public void backtrack(int i, int k, int n, int currSum){
-        if(curr.size() == k && currSum == n){
-            result.add(new HashSet<>(curr));
+    public void backtrack(int start, int k, int n, int currSum) {
+        if (curr.size() == k && currSum == n) {
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+        if (curr.size() > k || currSum > n) {
             return;
         }
         
-        for(int j=i; j<=9; j++){
-            if(currSum + j <= n){
-                curr.add(j);
-                backtrack(j+1, k, n, currSum + j);
-                curr.remove(curr.size() - 1);
-            }
+        for (int j = start; j <= 9; j++) {
+            curr.add(j);
+            backtrack(j + 1, k, n, currSum + j);
+            curr.remove(curr.size() - 1);
         }
     }
 }
