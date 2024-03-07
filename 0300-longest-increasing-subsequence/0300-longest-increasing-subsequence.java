9ad2e62e2eaef0bf97f59ae401ff654f871497dc
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-06 19:52:41 -0700

    Time: 392 ms (7.19%), Space: 44.8 MB (18.36%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,27 @@
+//top-bottom appraoch
+class Solution {
+    Map<Integer, Integer> hm = new HashMap<>();
+    public int lengthOfLIS(int[] nums) {
+        int ans = 0;
+        if(nums.length == 1)
+            return 1;
+        
+        for(int i=0; i<nums.length; i++)
+            ans = Math.max(ans, dp(i, nums));
+        
+        return ans;
+    }
+    
+    public int dp(int i, int[] nums){
+        if(hm.containsKey(i))
+            return hm.get(i);
+        
+        int ans=1;
+        for(int j=0; j<i; j++)
+            if(nums[j] < nums[i])
+                ans = Math.max(ans, dp(j, nums) + 1);
+        
+        hm.put(i, ans);
+        return ans;
+    }
+}
