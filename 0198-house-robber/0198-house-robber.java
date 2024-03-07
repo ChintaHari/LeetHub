dd59c8a59f6f2b6c31afeed36492a5763c17bda2
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-06 19:14:31 -0700

    Time: 0 ms (100.00%), Space: 41.1 MB (37.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,44 @@
-//top-borrom approach
+//top-bottom approach
+// class Solution {
+//     Map<Integer, Integer> hm = new HashMap<>();
+//     public int rob(int[] nums) {
+        
+//         if(nums.length == 1)
+//             return nums[0];
+        
+//         hm.put(0, nums[0]);
+//         hm.put(1, Math.max(nums[0], nums[1]));
+//         return dp(nums.length-1, nums);
+        
+//     }
+    
+//     public int dp(int i, int[] nums){
+//         if(i == 0 || i == 1)
+//             return hm.get(i);
+//         if(hm.containsKey(i))
+//             return hm.get(i);
+//         hm.put(i, Math.max(dp(i-2, nums) + nums[i], dp(i-1, nums)));
+        
+//         return hm.get(i);
+//     }   
+// }
+
+//bottom-up approach
 class Solution {
-    Map<Integer, Integer> hm = new HashMap<>();
     public int rob(int[] nums) {
         
         if(nums.length == 1)
-            return nums[0];
+             return nums[0];
+        int[] dp = new int[nums.length];
         
-        hm.put(0, nums[0]);
-        hm.put(1, Math.max(nums[0], nums[1]));
-        return dp(nums.length-1, nums);
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
         
+        for(int i=2; i<nums.length; i++){
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+        }
+        
+        return dp[dp.length-1];
     }
     
-    public int dp(int i, int[] nums){
-        if(i == 0 || i == 1)
-            return hm.get(i);
-        if(hm.containsKey(i))
-            return hm.get(i);
-        hm.put(i, Math.max(dp(i-2, nums) + nums[i], dp(i-1, nums)));
-        
-        return hm.get(i);
-    }   
 }
