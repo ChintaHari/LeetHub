b76e4ca688f76c27e61acdd377d5684be3bdbaf1
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-06 20:08:25 -0700

    Time: 51 ms (28.03%), Space: 44.1 MB (65.69%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,26 +1,47 @@
 //top-bottom appraoch
+// class Solution {
+//     Map<Integer, Integer> hm = new HashMap<>();
+//     public int lengthOfLIS(int[] nums) {
+//         int ans = 1;
+//         if(nums.length == 1)
+//             return ans;
+        
+//         for(int i=0; i<nums.length; i++)
+//             ans = Math.max(ans, dp(i, nums));
+        
+//         return ans;
+//     }
+//     public int dp(int i, int[] nums){
+//         if(hm.containsKey(i))
+//             return hm.get(i);
+        
+//         int ans=1;
+//         for(int j=0; j<i; j++)
+//             if(nums[j] < nums[i])
+//                 ans = Math.max(ans, dp(j, nums) + 1);
+        
+//         hm.put(i, ans);
+//         return ans;
+//     }
+// }
+
+//bottom-up appraoch
 class Solution {
-    Map<Integer, Integer> hm = new HashMap<>();
     public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        
         int ans = 1;
-        if(nums.length == 1)
-            return ans;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    ans = Math.max(ans, dp[i]);
+                }
+            }
+        }
         
-        for(int i=0; i<nums.length; i++)
-            ans = Math.max(ans, dp(i, nums));
-        
-        return ans;
-    }
-    public int dp(int i, int[] nums){
-        if(hm.containsKey(i))
-            return hm.get(i);
-        
-        int ans=1;
-        for(int j=0; j<i; j++)
-            if(nums[j] < nums[i])
-                ans = Math.max(ans, dp(j, nums) + 1);
-        
-        hm.put(i, ans);
         return ans;
     }
 }
