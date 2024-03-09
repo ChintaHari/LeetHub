9ef49b3814efc304a23323b9bb2cdc35b512b5f1
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-08 18:06:19 -0700

    Time: 0 ms (100.00%), Space: 40.4 MB (30.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,37 @@
+// top-bottom appraoch
+// class Solution {
+//     Map<Integer, Integer> hm = new HashMap<>();
+//     public int climbStairs(int n) {
+//         return dp(0, n);    
+//     }
+    
+//     public int dp(int index, int n){
+//         if(index > n)
+//             return 0;
+//         if(index == n)
+//             return 1;
+//         if(hm.containsKey(index))
+//             return hm.get(index);
+//         hm.put(index, dp(index + 1, n) + dp(index + 2, n));
+//         return hm.get(index);
+//     }
+// }
+
+//bottom-up appraoch
 class Solution {
-    Map<Integer, Integer> hm = new HashMap<>();
     public int climbStairs(int n) {
+        int[] dp = new int[n+1];
         
-        return dp(0, n);
-        
-    }
-    
-    public int dp(int index, int n){
-        if(index > n)
-            return 0;
-        if(index == n)
+        if(n==1)
             return 1;
-        if(hm.containsKey(index))
-            return hm.get(index);
-        hm.put(index, dp(index + 1, n) + dp(index + 2, n));
-        return hm.get(index);
+
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i <=n; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        
+        return dp[n];
+        
     }
-    
-    
 }
