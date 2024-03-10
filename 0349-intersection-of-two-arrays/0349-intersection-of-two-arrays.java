5decf4478c673a6d90d931e2d460edc89076b451
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-09 18:04:44 -0700

    Time: 9 ms (5.04%), Space: 43 MB (81.76%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -17,26 +17,41 @@
 // }
 
 
-class Solution{
-     public int[] intersection(int[] nums1, int[] nums2) {
-         Set<Integer> set = new HashSet<>();
-         Arrays.sort(nums1);
-         Arrays.sort(nums2);
-         int first = 0, second = 0;
+// class Solution{
+//      public int[] intersection(int[] nums1, int[] nums2) {
+//          Set<Integer> set = new HashSet<>();
+//          Arrays.sort(nums1);
+//          Arrays.sort(nums2);
+//          int first = 0, second = 0;
          
-         while(first < nums1.length && second < nums2.length){
-             if(nums1[first] < nums2[second])
-                 first ++;
-             else if(nums1[first] > nums2[second])
-                 second ++;
-             else{
-                 set.add(nums1[first]);
-                 first++;
-                 second++;
-             }
-         }
+//          while(first < nums1.length && second < nums2.length){
+//              if(nums1[first] < nums2[second])
+//                  first ++;
+//              else if(nums1[first] > nums2[second])
+//                  second ++;
+//              else{
+//                  set.add(nums1[first]);
+//                  first++;
+//                  second++;
+//              }
+//          }
          
-         return set.stream().mapToInt(Integer::intValue).toArray();
+//          return set.stream().mapToInt(Integer::intValue).toArray();
          
-     }
+//      }
+// }
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                if(nums1[i] == nums2[j]) set.add(nums1[i]);
+            }
+        }
+
+        int[] arr = set.stream().mapToInt(Integer::intValue).toArray();
+        return arr;
+    }
 }
