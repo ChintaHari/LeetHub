e7e47fbd14085981d64faee4940a7fb481cae69a
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-09 18:02:35 -0700

    Time: 5 ms (19.62%), Space: 43.3 MB (36.17%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,17 +1,42 @@
-class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> first = new HashSet<>();
-        Set<Integer> second = new HashSet<>();
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+//         Set<Integer> first = new HashSet<>();
+//         Set<Integer> second = new HashSet<>();
         
-        for(int ele : nums1)
-            first.add(ele);
+//         for(int ele : nums1)
+//             first.add(ele);
         
-        for(int ele : nums2)
-            second.add(ele);
+//         for(int ele : nums2)
+//             second.add(ele);
         
-        Set<Integer> intersectionSet = new HashSet<>(first);
-        intersectionSet.retainAll(second);
+//         Set<Integer> intersectionSet = new HashSet<>(first);
+//         intersectionSet.retainAll(second);
        
-        return intersectionSet.stream().mapToInt(Integer::intValue).toArray();
-    }
+//         return intersectionSet.stream().mapToInt(Integer::intValue).toArray();
+//     }
+// }
+
+
+class Solution{
+     public int[] intersection(int[] nums1, int[] nums2) {
+         Set<Integer> set = new HashSet<>();
+         Arrays.sort(nums1);
+         Arrays.sort(nums2);
+         int first = 0, second = 0;
+         
+         while(first < nums1.length && second < nums2.length){
+             if(nums1[first] < nums2[second])
+                 first ++;
+             else if(nums1[first] > nums2[second])
+                 second ++;
+             else{
+                 set.add(nums1[first]);
+                 first++;
+                 second++;
+             }
+         }
+         
+         return set.stream().mapToInt(Integer::intValue).toArray();
+         
+     }
 }
