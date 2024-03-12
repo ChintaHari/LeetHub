632db391191146c5862e21b644de58d44170532f
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-11 19:06:42 -0700

    Time: 2 ms (89.13%), Space: 44.4 MB (26.88%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -8,25 +8,52 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode removeZeroSumSublists(ListNode head) {
+        
+//         ListNode dummy = new ListNode(0, head);
+//         ListNode start = dummy;
+//         while(start != null){
+//             int prefixSum = 0;
+//             ListNode end = start.next;
+//             while(end != null){
+//                 prefixSum = prefixSum + end.val;
+//                 if(prefixSum == 0)
+//                     start.next = end.next;
+//                 end = end.next;
+//             }
+//             start = start.next;
+//         }
+//         return dummy.next;
+//     }
+// }
+
+
 class Solution {
     public ListNode removeZeroSumSublists(ListNode head) {
-        
         ListNode dummy = new ListNode(0, head);
-        ListNode start = dummy;
+        ListNode current = dummy;
+        Map<Integer, ListNode> hm = new HashMap<>();
+        hm.put(0, dummy);
         
-        while(start != null){
-            int prefixSum = 0;
-            ListNode end = start.next;
-            
-            while(end != null){
-                prefixSum = prefixSum + end.val;
-                if(prefixSum == 0)
-                    start.next = end.next;
-                end = end.next;
-            }
-            start = start.next;
+        int prefixSum = 0;
+        while(current != null){
+            prefixSum = prefixSum + current.val;
+            hm.put(prefixSum, current);
+            current = current.next;
+        }
+        
+        prefixSum = 0;
+        current = dummy;
+        
+        while(current != null){
+            prefixSum = prefixSum + current.val;
+            current.next = hm.get(prefixSum).next;
+            current = current.next;
         }
         
         return dummy.next;
+       
     }
 }
+
