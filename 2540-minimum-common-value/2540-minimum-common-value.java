f38f4770583742f032ed41c9dc2fd569a26dbc3f
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-14 16:25:34 -0700

    Time: 2 ms (81.09%), Space: 62.4 MB (68.42%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,11 +1,22 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        Set<Integer> set = new HashSet<>();
-        for(int ele : nums1)
-            set.add(ele);
-        for(int ele : nums2)
-            if(set.contains(ele))
-                return ele;
+        // Set<Integer> set = new HashSet<>();
+        // for(int ele : nums1)
+        //     set.add(ele);
+        // for(int ele : nums2)
+        //     if(set.contains(ele))
+        //         return ele;
+        // return -1;
+        
+        int first = 0, second = 0;
+        while(first < nums1.length  && second < nums2.length){
+            if(nums1[first] == nums2[second])
+                return nums1[first];
+            else if(nums1[first] < nums2[second])
+                first++;
+            else
+                second++;
+        }
         return -1;
         
     }
