d569dd351d9462080266a99f32cd2d6c84cad448
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-15 16:08:57 -0700

    Time: 43 ms (69.92%), Space: 62.5 MB (29.63%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
@@ -1,25 +1,17 @@
 class Solution {
     public int minimumCardPickup(int[] cards) {
-        HashMap<Integer, Integer> cardsMap = new HashMap<>();
-       int left=0, right=1;
-       int min = Integer.MAX_VALUE;
-       cardsMap.put(cards[0], 1);
-       for(right=1; right<cards.length; right++){
-           
-            while(cardsMap.containsKey(cards[right])){
-                min = Math.min(min, right - left + 1);
-                cardsMap.put(cards[left], cardsMap.get(cards[left])-1 );
-                if(cardsMap.get(cards[left]) == 0)
-                    cardsMap.remove(cards[left]);
-                left++;
-                
+        int ans = Integer.MAX_VALUE;
+        Map<Integer, Integer> lastSeen = new HashMap<>();
+
+        for (int right = 0; right < cards.length; right++) {
+            int card = cards[right];
+
+            if (lastSeen.containsKey(card)) {
+                int left = lastSeen.get(card);
+                ans = Math.min(ans, right - left + 1); 
             }
-             cardsMap.put(cards[right],cardsMap.getOrDefault(cards[right],0) + 1 );   
-          
-       }
-       if(min == Integer.MAX_VALUE)
-            return -1;
-        else
-            return min;
+            lastSeen.put(card, right);
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
