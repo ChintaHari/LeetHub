3c42d9c1f87e0098cd3ab8748ff3feb8db477b23
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-15 08:03:08 -0700

    Time: 1 ms (16.32%), Space: 41.4 MB (47.30%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,11 +1,19 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-        for(char ch : s.toCharArray()){
-            if(hm.containsKey(ch))
+        // HashMap<Character, Integer> hm = new HashMap<>();
+        // for(char ch : s.toCharArray()){
+        //     if(hm.containsKey(ch))
+        //         return ch;
+        //     hm.put(ch, hm.getOrDefault(ch,0) + 1);
+        // }
+        // return 'a';
+        
+        Set<Character> set = new HashSet<>();
+        for(char ch: s.toCharArray()){
+            if(set.contains(ch))
                 return ch;
-            hm.put(ch, hm.getOrDefault(ch,0) + 1);
+            set.add(ch);
         }
-        return 'a';
+        return 'c';
     }
 }
