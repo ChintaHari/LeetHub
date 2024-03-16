068b80f87c91da630e07de2188f731b07435d054
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-15 22:52:21 -0700

    Time: 49 ms (74.88%), Space: 60.9 MB (22.41%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -1,23 +1,43 @@
-class Solution {
-    public int maximumUniqueSubarray(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        int ans = 0, currSum = 0, left = 0, right = 0;
-        for(right = 0; right < nums.length; right++){
-           hm.put(nums[right], hm.getOrDefault(nums[right], 0) + 1);
+// class Solution {
+//     public int maximumUniqueSubarray(int[] nums) {
+//         HashMap<Integer, Integer> hm = new HashMap<>();
+//         int ans = 0, currSum = 0, left = 0, right = 0;
+//         for(right = 0; right < nums.length; right++){
+//            hm.put(nums[right], hm.getOrDefault(nums[right], 0) + 1);
             
-            while(left < nums.length && hm.getOrDefault(nums[right],0) > 1){
-                //currSum = currSum - nums[right];
-                ans = Math.max(ans, currSum);
-                currSum = currSum - nums[left];
-                hm.put(nums[left], hm.getOrDefault(nums[left], 0) - 1);
-                if(hm.get(nums[left]) < 1)
-                    hm.remove(nums[left]);
-                left++;
+//             while(left < nums.length && hm.getOrDefault(nums[right],0) > 1){
+//                 //currSum = currSum - nums[right];
+//                 ans = Math.max(ans, currSum);
+//                 currSum = currSum - nums[left];
+//                 hm.put(nums[left], hm.getOrDefault(nums[left], 0) - 1);
+//                 if(hm.get(nums[left]) < 1)
+//                     hm.remove(nums[left]);
+//                 left++;
+//             }
+            
+//             currSum = currSum + nums[right];
+//             ans = Math.max(ans, currSum);
+//         }
+//         return ans;
+//     }
+// }
+
+    class Solution {
+        public int maximumUniqueSubarray(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            int sum = 0;
+            int max = Integer.MIN_VALUE;
+            int i=0, j=0;
+            for(j=0; j<nums.length; j++){
+                sum = sum + nums[j];
+                while(set.contains(nums[j])){
+                    sum = sum - nums[i];
+                    set.remove(nums[i]);
+                    i++;
+                }
+                set.add(nums[j]);
+                max = Math.max(max, sum);
             }
-            
-            currSum = currSum + nums[right];
-            ans = Math.max(ans, currSum);
+            return max;
         }
-        return ans;
     }
-}
