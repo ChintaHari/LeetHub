652f7bd33d21ae46f42c40daf510dbeb5de10ac0
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-16 11:09:10 -0700

    Time: 2 ms (70.11%), Space: 41.5 MB (45.25%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,23 +1,22 @@
 class Solution {
-    HashMap<Character, Integer> hm = new HashMap<>();
     public String customSortString(String order, String s) {
-        StringBuffer sb = new StringBuffer();
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(char ch : s.toCharArray())
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
         
-        for(char ch: s.toCharArray())
-            hm.put(ch, hm.getOrDefault(ch,0)+1);
-        
-        for(char ch1: order.toCharArray()){
-            for(char ch2: s.toCharArray()){
-                if(ch2 == ch1){
+        StringBuilder sb = new StringBuilder();
+        for(char ch :  order.toCharArray()){
+            for(char ch2 : s.toCharArray()){
+                if(ch2 == ch){
                     sb.append(new String(new char[hm.get(ch2)]).replace('\0', ch2));
-                    hm.remove(ch2);    
+                    hm.remove(ch2);
                     break;
                 }
             }
         }
         
-        for(char ch : hm.keySet())
-            sb.append(new String(new char[hm.get(ch)]).replace('\0', ch));
+        for(char key : hm.keySet())
+            sb.append(new String(new char[hm.get(key)]).replace('\0', key));
         
         return sb.toString();
     }
