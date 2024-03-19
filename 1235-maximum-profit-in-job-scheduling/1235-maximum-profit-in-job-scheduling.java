46b35624f154bc0da1167d17c3df1cf909c28ad0
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-18 21:48:19 -0700

    Time: 1085 ms (9.65%), Space: 57.4 MB (42.48%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,3 +1,10 @@
+//Process each startTime, endTime and profit into tuple
+//Sort based on end times
+//Start dp (initialize dp[0] = first job's profit after sorting)
+//now iterate from 2nd job. If I have to take that job, I have to check if there is non-overlapping job previously.
+//If yes, add the profit of that till previously non-overlapping job to the current job's profit.
+//If you don't find it, just add the max(dp[i-1], current job's profit) to the dp[i];
+
 class Job{
     int start, end, profit;
     Job(int start, int end, int profit){
@@ -7,7 +14,7 @@ class Job{
     }
 }
 class Solution {
-    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+    public int jobScheduling(int[] startTime, int[] endTime , int[] profit) {
         Job[] jobs = new Job[startTime.length];
         
         for(int i=0; i<startTime.length; i++)
