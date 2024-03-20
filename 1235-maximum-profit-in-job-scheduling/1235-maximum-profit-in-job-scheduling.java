2e117e8145eeda0cd658d5ba323efffa2ea1cda7
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-19 21:55:33 -0700

    Time: 1190 ms (8.57%), Space: 56 MB (63.58%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,10 +1,3 @@
-//Process each startTime, endTime and profit into tuple
-//Sort based on end times
-//Start dp (initialize dp[0] = first job's profit after sorting)
-//now iterate from 2nd job. If I have to take that job, I have to check if there is non-overlapping job previously.
-//If yes, add the profit of that till previously non-overlapping job to the current job's profit.
-//If you don't find it, just add the max(dp[i-1], current job's profit) to the dp[i];
-
 class Job{
     int start, end, profit;
     Job(int start, int end, int profit){
@@ -14,28 +7,27 @@ class Job{
     }
 }
 class Solution {
-    public int jobScheduling(int[] startTime, int[] endTime , int[] profit) {
-        Job[] jobs = new Job[startTime.length];
-        
-        for(int i=0; i<startTime.length; i++)
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        Job[] jobs = new Job[n];
+        for(int i=0; i<n; i++)
             jobs[i] = new Job(startTime[i], endTime[i], profit[i]);
         
-        Arrays.sort(jobs, (a, b) -> a.end - b.end);
+        Arrays.sort(jobs, (a,b) -> a.end - b.end);
         
-        int dp[] = new int[startTime.length];
+        int[] dp = new int[n];
         dp[0] = jobs[0].profit;
         
-        for(int i=1; i<dp.length; i++){
-            int proftIncludingCurrent = jobs[i].profit;
+        for(int i=1; i<n; i++){
+            int profitOfTheCurrentInterval = jobs[i].profit;
             for(int j=i-1; j>=0; j--){
                 if(jobs[i].start >= jobs[j].end){
-                    proftIncludingCurrent = Math.max(proftIncludingCurrent, proftIncludingCurrent + dp[j]);
+                    profitOfTheCurrentInterval = profitOfTheCurrentInterval + dp[j];
                     break;
                 }
             }
-            dp[i] = Math.max(dp[i-1], proftIncludingCurrent);
+            dp[i] = Math.max(dp[i-1], profitOfTheCurrentInterval);
         }
-        
-        return dp[dp.length-1];
+        return dp[n-1];
     }
 }
