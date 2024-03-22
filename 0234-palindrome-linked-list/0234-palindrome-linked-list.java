cfd786d567f4d2483597de15951758d21dd9dea3
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-22 09:27:36 -0700

    Time: 9 ms (25.36%), Space: 63.8 MB (63.83%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,18 +10,60 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stack = new Stack<>();
-        ListNode temp = head;
-        while(temp != null){
-            stack.push(temp.val);
-            temp = temp.next;
+        if(head == null || head.next == null)
+            return true;
+        
+        ListNode curr = head;
+        
+        ListNode copyHead = copyList(head);
+
+        ListNode reverse = reverseList(copyHead);
+        curr = head;
+        
+        while(curr != null && reverse != null){
+            if(curr.val != reverse.val)
+                return false;
+            curr = curr.next;
+            reverse = reverse.next;
         }
         
-        while(head != null){
-            if(head.val != stack.pop())
-                return false;
-            head = head.next;
-        }
         return true;
+        
+        
+//         Stack<Integer> stack = new Stack<>();
+//         ListNode temp = head;
+//         while(temp != null){
+//             stack.push(temp.val);
+//             temp = temp.next;
+//         }
+        
+//         while(head != null){
+//             if(head.val != stack.pop())
+//                 return false;
+//             head = head.next;
+//         }
+//         return true;
+    }
+    
+    public ListNode reverseList(ListNode node){
+        ListNode prev = null;
+        while(node != null){
+            ListNode nextNode = node.next;
+            node.next = prev;
+            prev = node;
+            node = nextNode;
+        }
+        return prev;
+    }
+    
+    private ListNode copyList(ListNode node) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while(node != null) {
+            tail.next = new ListNode(node.val);
+            tail = tail.next;
+            node = node.next;
+        }
+        return dummy.next;
     }
 }
