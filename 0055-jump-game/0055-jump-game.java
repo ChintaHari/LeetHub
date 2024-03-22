7855bb23bed5a88761b5db4bc714192bfb723c03
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-22 12:05:11 -0700

    Time: 65 ms (18.94%), Space: 45.1 MB (94.45%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,30 +1,49 @@
-class Solution {
-    int[] memo, nums;
-    int n;
-    public boolean canJump(int[] nums) {
-        memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        this.nums = nums;
-        n = nums.length;
-        return canReachEnd(0);
-    }
+// class Solution {
+//     int[] memo, nums;
+//     int n;
+//     public boolean canJump(int[] nums) {
+//         memo = new int[nums.length];
+//         Arrays.fill(memo, -1);
+//         this.nums = nums;
+//         n = nums.length;
+//         return canReachEnd(0);
+//     }
     
-    public boolean canReachEnd(int position){
-        if(position == n-1)
-            return true;
+//     public boolean canReachEnd(int position){
+//         if(position == n-1)
+//             return true;
         
-        if(memo[position] != -1)
-            return memo[position] == 1;
+//         if(memo[position] != -1)
+//             return memo[position] == 1;
         
-        int farthestPositionThatCanBeReachedByCurrentPosition = Math.min(position + nums[position], n-1);
-        for(int nextPosition = position + 1; nextPosition <= farthestPositionThatCanBeReachedByCurrentPosition; nextPosition++){
-            if(canReachEnd(nextPosition)){
-                memo[nextPosition] = 1;
-                return true;
+//         int farthestPositionThatCanBeReachedByCurrentPosition = Math.min(position + nums[position], n-1);
+//         for(int nextPosition = position + 1; nextPosition <= farthestPositionThatCanBeReachedByCurrentPosition; nextPosition++){
+//             if(canReachEnd(nextPosition)){
+//                 memo[nextPosition] = 1;
+//                 return true;
+//             }
+//         }
+        
+//         memo[position] = 0;
+//         return false;
+//     }
+// }
+
+
+//bottom-up appraoch
+class Solution {
+    public boolean canJump(int[] nums) {
+        boolean[] dp = new boolean[nums.length];
+        dp[nums.length - 1] = true;
+        for(int i = nums.length - 2; i>=0 ; i--){
+            int farthestPositionThatCanBeReachedByCurrentPosition = Math.min(i + nums[i], nums.length-1);
+            for(int nP = i+1; nP <= farthestPositionThatCanBeReachedByCurrentPosition; nP++){
+                if(dp[nP]){
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        
-        memo[position] = 0;
-        return false;
+        return dp[0];
     }
 }
