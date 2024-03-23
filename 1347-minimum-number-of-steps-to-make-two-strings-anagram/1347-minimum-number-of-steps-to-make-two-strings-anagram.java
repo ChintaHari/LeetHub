f245d2b5c12a109e151116386b9d9bb72a7c2c03
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-23 16:58:48 -0700

    Time: 63 ms (16.28%), Space: 45.2 MB (69.90%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,19 +1,19 @@
 class Solution {
     public int minSteps(String s, String t) {
+        
         HashMap<Character, Integer> sMap = new HashMap<>();
         HashMap<Character, Integer> tMap = new HashMap<>();
         
-        int count =0;
-        for(char ch : s.toCharArray())
-            sMap.put(ch, sMap.getOrDefault(ch, 0) + 1);
+        for(int i=0; i<s.length(); i++){
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i) ,0)+1);
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i) ,0)+1);
+        }
         
-        for(char ch: t.toCharArray())
-             tMap.put(ch, tMap.getOrDefault(ch, 0) + 1);
-        
+        int count = 0;
         for(char key : sMap.keySet()){
             int needed = sMap.get(key);
-            int availableOnHand = tMap.getOrDefault(key, 0);
-            count = count + Math.max(0, needed - availableOnHand);
+            int available = tMap.getOrDefault(key, 0);
+            count = count + Math.max(0, needed - available);
         }
         return count;
     }
