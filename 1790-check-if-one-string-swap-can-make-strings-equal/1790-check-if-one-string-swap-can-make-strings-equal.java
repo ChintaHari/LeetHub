14587f8dd953fcd75cea552f3d2163fc81e057e8
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-23 13:14:02 -0700

    Time: 1 ms (51.03%), Space: 41.7 MB (15.80%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,17 +1,15 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-       if(s1.length() != s2.length())
-           return false;
-        
-        List<Integer> differencesInIndices = new ArrayList<>();
+        List<Integer> differentIndices = new ArrayList<>();
         for(int i=0; i<s1.length(); i++)
             if(s1.charAt(i) != s2.charAt(i))
-                differencesInIndices.add(i);
-        if(differencesInIndices.isEmpty())
+                differentIndices.add(i);
+        
+        if(differentIndices.size() == 0)
             return true;
-        else if(differencesInIndices.size() == 2){
-            if(s1.charAt(differencesInIndices.get(0)) == s2.charAt(differencesInIndices.get(1)) && 
-              s1.charAt(differencesInIndices.get(1)) == s2.charAt(differencesInIndices.get(0)))
+        else if(differentIndices.size() == 2){
+            if(s1.charAt(differentIndices.get(0)) == s2.charAt(differentIndices.get(1))  &&
+              s1.charAt(differentIndices.get(1)) == s2.charAt(differentIndices.get(0)))
                 return true;
             else
                 return false;
