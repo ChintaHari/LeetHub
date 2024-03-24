e70f12911075805a644e13d9003c0703b61bcf85
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-24 14:08:00 -0700

    Time: 1 ms (24.63%), Space: 39.8 MB (95.90%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,15 +1,15 @@
+//watch : https://www.youtube.com/watch?v=p1tvA-eQFqk
 class Solution {
     public int countOrders(int n) {
-        int mod = (int)Math.pow(10, 9) + 7;
         long[] dp = new long[501];
-        dp[1] = (long)1;
-        dp[2] = (long)6;
-        for(int i=3; i<=n; i++){
-            int oddNumber = (2*i) - 1;
-            int totalNumberOfWays = (int)(oddNumber * (oddNumber + 1))/2;
-            dp[i] = (totalNumberOfWays * dp[i-1]) % mod;
+        int mod = (int)Math.pow(10, 9) + 7;
+        dp[1] = 1L;
+        dp[2] = 6L;
+        for(int i=2; i<dp.length; i++){
+            int oddNumber = (2 * i) - 1;
+            int numberOfWays = (oddNumber * (oddNumber + 1)/2);
+            dp[i] = (numberOfWays * dp[i-1]) % mod;
         }
         return (int)dp[n];
-        
     }
 }
