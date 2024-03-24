69e67b00447fd73ac99f546af686ad315391e277
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-24 16:28:57 -0700

    Time: 54 ms (38.40%), Space: 50.1 MB (93.97%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -1,27 +1,27 @@
 class BrowserHistory {
-    List<String> browserHistory;
+    List<String> browser;
     int currentIndex;
 
     public BrowserHistory(String homepage) {
-        browserHistory = new ArrayList<>();
-        browserHistory.add(homepage);
+        browser = new ArrayList<>();
+        browser.add(homepage);
         currentIndex = 0;
     }
     
     public void visit(String url) {
-        browserHistory.subList(currentIndex + 1, browserHistory.size()).clear();
-        browserHistory.add(url);
+        browser.subList(currentIndex + 1, browser.size()).clear();
+        browser.add(url);
         currentIndex++;
     }
     
     public String back(int steps) {
         currentIndex = Math.max(0, currentIndex - steps);
-        return browserHistory.get(currentIndex);
+        return browser.get(currentIndex);
     }
     
     public String forward(int steps) {
-        currentIndex = Math.min(browserHistory.size() - 1, currentIndex + steps);
-        return browserHistory.get(currentIndex);
+        currentIndex = Math.min(browser.size() -1, currentIndex + steps);
+        return browser.get(currentIndex);
     }
 }
 
