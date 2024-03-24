50d061302c619b7acba6934bbe03f3cc1bbb02a7
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-24 14:56:55 -0700

    Time: 44 ms (32.06%), Space: 58 MB (71.77%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -1,6 +1,8 @@
 //Key point: We need to increase the multiplication rate that gives us maximum team performance.
 //To increase the rate, we need to focus on what we are multiplying. here it is efficiency.
 //So Construct 2D array and sort it based on decreasing efficiency. Such that we can start with highest efficiency first and keep on updating the max team performance later.
+//Watch : https://www.youtube.com/watch?v=FW73Sxo0YMA&t=807s
+
 class Solution {
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
         List<int[]> list = new ArrayList<>();
