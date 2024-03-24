5a9b2cc5e1b50df9c44b73f46c74cf01f67b77b0
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-24 16:18:00 -0700

    Time: 1159 ms (51.98%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,22 +1,13 @@
 # Write your MySQL query statement below
-
-#Step 1: Find all the customer's first orders.
-#Step 2: From the result obtained from step 1, filter those records whose order date = preferred delivery date
-#(Hint for step 2: Use join)
-#Step 3: Now calculate the percentage of step1 with step 2
-
-with first_orders as(
+with first_orders as (
     select customer_id, min(order_date) as firstOrderDate from Delivery group by customer_id
 ),
 
-immediate_first_orders as(
-    select first_orders.customer_id from first_orders 
-    JOIN Delivery ON 
-    first_orders.customer_id = Delivery.customer_id 
-    and   
+immediate_first_orders as (
+    select first_orders.customer_id from first_orders JOIN Delivery
+    on first_orders.customer_id = Delivery.customer_id and
     first_orders.firstOrderDate = Delivery.order_date
     where Delivery.order_date = Delivery.customer_pref_delivery_date
 )
 
-select round((count(*) * 100) / (select count(*) from first_orders), 2) as immediate_percentage from immediate_first_orders
-
+select round( count(*) * 100 / (select count(*) from first_orders), 2) as immediate_percentage from immediate_first_orders;
