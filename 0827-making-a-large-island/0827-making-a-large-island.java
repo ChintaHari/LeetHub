bb080d3661d7ce745f99894d7560d87e93d35417
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-23 20:13:33 -0700

    Time: 96 ms (67.82%), Space: 73.6 MB (80.02%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.java
+++ 0827-making-a-large-island/0827-making-a-large-island.java
@@ -1,64 +1,65 @@
 class Solution {
     int[][] grid;
-    int rows, cols;
-    int maxSizeOfIsland = 0;
+    int rows = 0, cols=0;
+    boolean[][] visited;
+    int[][] dirs = new int[][]{{0,1}, {1,0}, {-1,0}, {0,-1}};
+    Map<Integer, Integer> islandIDtoSizeMap = new HashMap<>();
+    int islandID = 2, numberOfIslands = 0, maxIsland = 0;
     boolean hasZero = false;
-    int numOfIslands = 0;
-    int islandID = 2;
-    int[][] dirs = new int[][]{{0,1}, {1,0}, {0,-1}, {-1,0}};
-    HashMap<Integer, Integer> islandIDtoSizeMap = new HashMap<>();
     public int largestIsland(int[][] grid) {
         this.grid = grid;
         rows = grid.length;
         cols = grid[0].length;
+        visited = new boolean[rows][cols];
         
-        for(int row=0; row < rows; row++){
-            for(int col=0; col < cols; col++){
+        for(int row = 0; row < rows ; row++){
+            for(int col = 0; col<cols; col++){
                 if(grid[row][col] == 1){
-                    int size = dfs(row, col, islandID); //this is the start of an island, so find the size of this island through DFS
-                    numOfIslands++;
+                    int size = dfs(row, col, islandID);
+                    numberOfIslands++;
                     islandIDtoSizeMap.put(islandID, size);
-                    maxSizeOfIsland = Math.max(maxSizeOfIsland, size);
-                    islandID++;
+                    maxIsland = Math.max(maxIsland, size);
+                    islandID++;;
                 }
             }
         }
         
-        for(int row=0; row < rows; row++){
-            for(int col=0; col<cols; col++){
+        for(int row=0; row<rows; row++){
+            for(int col = 0; col<cols; col++){
                 if(grid[row][col] == 0){
                     hasZero = true;
-                    Set<Integer> neighbouringIslandIDs = new HashSet<>();
+                    HashSet<Integer> set = new HashSet<>();
                     for(int[] dir : dirs){
                         int nextRow = row + dir[0];
                         int nextCol = col + dir[1];
-                        
-                        if(nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols && grid[nextRow][nextCol] >= 1)
-                            neighbouringIslandIDs.add(grid[nextRow][nextCol]);
-                        
-                        int size = 1;
-                        
-                        for(int id : neighbouringIslandIDs)
-                            size = size + islandIDtoSizeMap.get(id);
-                        
-                         maxSizeOfIsland = Math.max(maxSizeOfIsland, size);
+                        if(nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols && grid[nextRow][nextCol] > 1)
+                            set.add(grid[nextRow][nextCol]);
                     }
+                    int size = 1;
+                    for(int id : set)
+                        size = size + islandIDtoSizeMap.get(id);
+                    maxIsland = Math.max(maxIsland, size);
+
                 }
             }
         }
         
-        return hasZero ? maxSizeOfIsland : rows * cols;
+        return hasZero ? maxIsland : rows * cols;
     }
     
-    
     public int dfs(int row, int col, int islandID){
+        visited[row][col] = true;
         grid[row][col] = islandID;
         int size = 1;
+        
         for(int[] dir : dirs){
             int nextRow = row + dir[0];
             int nextCol = col + dir[1];
-            if(nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols && grid[nextRow][nextCol] == 1)
+            
+            if(nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols 
+               && !visited[nextRow][nextCol] && grid[nextRow][nextCol] == 1){
                 size = size + dfs(nextRow, nextCol, islandID);
+            }
         }
         return size;
     }
