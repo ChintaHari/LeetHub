3ea6eb81a560e71010965b70d8ee1303d92531e9
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-24 23:47:01 -0700

    Time: 2 ms (85.02%), Space: 51.8 MB (94.68%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -21,11 +21,13 @@ class Solution {
     public int dfs(TreeNode node, int maxSoFar){
         if(node == null)
             return 0;
+        int ans = 0;
+         if(node.val >= maxSoFar)
+            ans = ans + 1;
         int left = dfs(node.left, Math.max(maxSoFar,  node.val));
         int right = dfs(node.right, Math.max(maxSoFar, node.val));
-        int ans = left + right;
-        if(node.val >= maxSoFar)
-            ans = ans + 1;
+        ans = ans + left + right;
+       
         return ans;
     }
 }
