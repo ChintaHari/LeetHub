26e840cefd517a5560ec6e90d8afeaeb7135a1f3
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-26 20:42:47 -0700

    Time: 11 ms (35.13%), Space: 41.2 MB (34.46%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -32,7 +32,6 @@ class Solution {
         for(int i=0; i<9; i++){
             if(board[i][col] == ch) return false;
             if(board[row][i] == ch) return false;
-            System.out.println(row + " " +col + " "+ i);
             if(board[(3 * (row/3)) + i/3][(3 * (col/3)) + i%3] == ch) return false;
         }
         return true;
