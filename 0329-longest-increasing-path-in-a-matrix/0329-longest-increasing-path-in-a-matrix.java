ca3b1a5b702ced54f0b4b451819ad8af55c20c40
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-26 20:11:09 -0700

    Time: 8 ms (80.40%), Space: 45.1 MB (38.08%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,38 +1,40 @@
 class Solution {
-    int[][] matrix, memo;
-    int rows = 0, cols=0;
-    int longestPath = 0;
-    int[][] dirs = new int[][]{{1,0}, {0,1}, {-1,0}, {0,-1}};
+    int[][] memo;
+    int rows, cols;
+    int[][] matrix;
+    int maxPath = 0;
+    int [][] dirs = new int[][]{{0,1}, {1,0}, {-1, 0}, {0, -1}};
     public int longestIncreasingPath(int[][] matrix) {
         rows = matrix.length;
         cols = matrix[0].length;
-        this.matrix = matrix;
         memo = new int[rows][cols];
+        this.matrix = matrix;
         
-        for(int row=0; row < rows; row++){
-            for(int col=0; col<cols; col++){
-                longestPath = Math.max(longestPath, findMaxPathForThisRowAndColumn(row, col));
+        for(int row = 0; row < rows; row++){
+            for(int col =0; col < cols; col++){
+               maxPath = Math.max(maxPath, longestPathForThisCurrentRowAndCol(row, col));
             }
         }
         
-        return longestPath;
+        return maxPath;
     }
     
-    public int findMaxPathForThisRowAndColumn(int row, int col){
+    public int longestPathForThisCurrentRowAndCol(int row, int col){
         if(memo[row][col] != 0)
             return memo[row][col];
         
-        int currentMaxPathForThisRowAndColumn = 1;
+        int currentMaxPathForThisCell = 1;
         for(int[] dir : dirs){
             int nextRow = row + dir[0];
             int nextCol = col + dir[1];
             
-            if(nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols && matrix[nextRow][nextCol] > matrix[row][col]){
-                int path = 1 + findMaxPathForThisRowAndColumn(nextRow, nextCol);
-                currentMaxPathForThisRowAndColumn = Math.max(currentMaxPathForThisRowAndColumn, path);
+            if(nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols 
+               && matrix[nextRow][nextCol] > matrix[row][col]){
+                int path = 1 + longestPathForThisCurrentRowAndCol(nextRow, nextCol);
+                currentMaxPathForThisCell = Math.max(currentMaxPathForThisCell, path);
             }
         }
-        memo[row][col] = currentMaxPathForThisRowAndColumn;
-        return memo[row][col];
+        memo[row][col] = currentMaxPathForThisCell;
+        return currentMaxPathForThisCell;
     }
 }
