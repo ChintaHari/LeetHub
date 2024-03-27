1744a632cf3c94a539244ee5feb0a08593276455
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-26 20:42:18 -0700

    Time: 1963 ms (5.20%), Space: 45.4 MB (5.65%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,36 +1,39 @@
 class Solution {
-    int rows = 0, cols =0;
+    int rows, cols;
     public void solveSudoku(char[][] board) {
         rows = board.length;
         cols = board[0].length;
+        
         solve(board);
     }
     
     public boolean solve(char[][] board){
-        for(int row=0; row < rows; row++){
-            for(int col=0; col<cols; col++){
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
                 if(board[row][col] == '.'){
                     for(char ch = '1'; ch <= '9'; ch++){
-                        if(isValid(board, ch, row, col)){
+                        if(isValid(ch, row, col, board)){
                             board[row][col] = ch;
                             if(solve(board))
                                 return true;
                             else
-                               board[row][col] = '.'; 
+                                board[row][col] = '.';
                         }
                     }
                     return false;
                 }
             }
         }
+        
         return true;
     }
     
-    public boolean isValid(char[][] board, char ch, int row, int col){
+    public boolean isValid(char ch, int row, int col, char[][] board){
         for(int i=0; i<9; i++){
             if(board[i][col] == ch) return false;
             if(board[row][i] == ch) return false;
-            if(board[3 * (row/3) + i/3][3 * (col/3) + i%3] == ch) return false;
+            System.out.println(row + " " +col + " "+ i);
+            if(board[(3 * (row/3)) + i/3][(3 * (col/3)) + i%3] == ch) return false;
         }
         return true;
     }
