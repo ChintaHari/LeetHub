8223ee01223d27841fa0a59717d88d1777f4ff93
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-27 18:55:21 -0700

    Time: 3 ms (30.16%), Space: 49.2 MB (67.59%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.java
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.java
@@ -1,17 +1,18 @@
 class Solution {
     public int nearestValidPoint(int x, int y, int[][] points) {
-        int smallestIndex = -1; 
-        int minDistance = Integer.MAX_VALUE ;
-        for(int i=0; i<points.length; i++){
-            if(points[i][0] == x || points[i][1] == y){
-                int distance = Math.abs(points[i][0] - x) + Math.abs(points[i][1] - y);
-                if(distance < minDistance){
-                    minDistance = distance;
-                    smallestIndex = i;
+        int minManhattenDistance = Integer.MAX_VALUE;
+        int minIndex = -1;
+        for(int i = 0; i< points.length; i++){
+            int xi = points[i][0];
+            int yi = points[i][1];
+            if(xi == x || yi == y){
+                int distance = Math.abs(x-xi) + Math.abs(y-yi);
+                if(distance < minManhattenDistance){
+                    minManhattenDistance = distance;
+                    minIndex = i;
                 }
             }
         }
-        
-        return smallestIndex;
+        return minIndex;
     }
 }
