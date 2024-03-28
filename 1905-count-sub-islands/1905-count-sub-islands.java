d7af0ccdc5420998b8d1b6942e2c40d07ade78ac
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-03-27 22:04:50 -0700

    Time: 25 ms (47.17%), Space: 87.3 MB (22.87%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,13 +1,20 @@
 class Solution {
+    int[][] grid1, grid2;
+    int m, n;
+    boolean[][] visited;
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        int m = grid2.length, n = grid2[0].length;
-        boolean[][] visited = new boolean[m][n];
+        m = grid2.length;
+        n = grid2[0].length;
+        visited = new boolean[m][n];
+        
+        this.grid1 = grid1;
+        this.grid2 = grid2;
         int count = 0;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid2[i][j] == 1 && !visited[i][j]) {
-                    if (dfs(grid1, grid2, visited, i, j)) {
+                    if (dfs(i, j)) {
                         count++; 
                     }
                 }
@@ -17,8 +24,8 @@ class Solution {
         return count;
     }
     
-    private boolean dfs(int[][] grid1, int[][] grid2, boolean[][] visited, int i, int j) {
-        if (i < 0 || i >= grid2.length || j < 0 || j >= grid2[0].length || grid2[i][j] == 0 || visited[i][j]) 
+    private boolean dfs(int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid2[i][j] == 0 || visited[i][j]) 
             return true;
         
         visited[i][j] = true;
@@ -27,10 +34,10 @@ class Solution {
         if (grid1[i][j] == 0) 
             result = false;
 
-        result &= dfs(grid1, grid2, visited, i + 1, j);
-        result &= dfs(grid1, grid2, visited, i - 1, j);
-        result &= dfs(grid1, grid2, visited, i, j + 1);
-        result &= dfs(grid1, grid2, visited, i, j - 1);
+        result &= dfs(i + 1, j);
+        result &= dfs(i - 1, j);
+        result &= dfs(i, j + 1);
+        result &= dfs(i, j - 1);
         
         return result;
     }
