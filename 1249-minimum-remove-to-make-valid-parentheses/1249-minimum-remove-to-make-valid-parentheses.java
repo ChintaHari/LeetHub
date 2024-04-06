217e2864d7e597310f526b83bd9f47e0dbd8251f
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-04-05 17:34:17 -0700

    Time: 22 ms (50.70%), Space: 45.6 MB (29.12%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,52 +1,52 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-//         StringBuilder sb = new StringBuilder();
-//         int open = 0;
-//         for (char c : s.toCharArray()) {
-//             if (c == '(') 
-//                 open++;
-            
-//             else if (c == ')') {
-//                 if (open == 0) 
-//                     continue; 
-//                 open--;
-//             }
-//             sb.append(c);
-//         }
-        
-//         System.out.println(sb);
-        
-//         StringBuilder result = new StringBuilder();
-//         for (int i = sb.length() - 1; i >= 0; i--) {
-//             if (sb.charAt(i) == '(' && open > 0) {
-//                 open --;
-//                 continue;
-//             } 
-//             result.append(sb.charAt(i)); 
-//         }
-        
-//         return result.reverse().toString();
-        
         StringBuilder sb = new StringBuilder();
-        Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            switch (s.charAt(i)) {
-                case '(':
-                    st.push(i);
-                    break;
-                case ')':
-                    if (!st.isEmpty() && s.charAt(st.peek()) == '(') {
-                        st.pop();
-                    } else {
-                        st.push(i);
-                    }
-                    break;
+        int open = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') 
+                open++;
+            
+            else if (c == ')') {
+                if (open == 0) 
+                    continue; 
+                open--;
             }
-            sb.append(s.charAt(i));
+            sb.append(c);
         }
-        while (!st.isEmpty()) {
-            sb.deleteCharAt(st.pop());
+        
+        System.out.println(sb);
+        
+        StringBuilder result = new StringBuilder();
+        for (int i = sb.length() - 1; i >= 0; i--) {
+            if (sb.charAt(i) == '(' && open > 0) {
+                open --;
+                continue;
+            } 
+            result.append(sb.charAt(i)); 
         }
-        return sb.toString();
+        
+        return result.reverse().toString();
     }
 }
+
+        // StringBuilder sb = new StringBuilder();
+        // Stack<Integer> st = new Stack<>();
+        // for (int i = 0; i < s.length(); i++) {
+        //     switch (s.charAt(i)) {
+        //         case '(':
+        //             st.push(i);
+        //             break;
+        //         case ')':
+        //             if (!st.isEmpty() && s.charAt(st.peek()) == '(') {
+        //                 st.pop();
+        //             } else {
+        //                 st.push(i);
+        //             }
+        //             break;
+        //     }
+        //     sb.append(s.charAt(i));
+        // }
+        // while (!st.isEmpty()) {
+        //     sb.deleteCharAt(st.pop());
+        // }
+        // return sb.toString();
