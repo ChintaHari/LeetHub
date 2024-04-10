c12f11727c7591613d035d16bffcf1d12e7f6a0a
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-04-10 15:17:08 -0700

    Time: 24 ms (55.36%), Space: 47.4 MB (52.53%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,22 +1,48 @@
+// class Solution {
+//     public int findShortestSubArray(int[] nums) {
+//         HashMap<Integer, Integer> hm = new HashMap<>();
+//         for(int num : nums)
+//             hm.put(num, hm.getOrDefault(num,0) + 1);
+        
+//         int maxDegree = hm.values().stream().max(Integer::compareTo).orElse(0);
+//         System.out.println(maxDegree);
+//         List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        
+//         int minSubArrayLength = Integer.MAX_VALUE;
+//         for(int i=0; i<nums.length; i++){
+//             if(hm.get(nums[i]) == maxDegree){
+//                 minSubArrayLength = Math.min(minSubArrayLength, 
+//                                               numsList.lastIndexOf(nums[i]) - numsList.indexOf(nums[i]) + 1);
+//             }
+//         }
+        
+//         return minSubArrayLength == Integer.MAX_VALUE ? 0 : minSubArrayLength;
+        
+//     }
+// }
+
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int num : nums)
-            hm.put(num, hm.getOrDefault(num,0) + 1);
+        HashMap<Integer, Integer> leftMostIndexOfElementMap = new HashMap<>();
+        HashMap<Integer, Integer> rightMostIndexOfElementMap = new HashMap<>();
+        HashMap<Integer, Integer> count = new HashMap<>();
         
-        int maxDegree = hm.values().stream().max(Integer::compareTo).orElse(0);
-        System.out.println(maxDegree);
-        List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        
-        int minSubArrayLength = Integer.MAX_VALUE;
         for(int i=0; i<nums.length; i++){
-            if(hm.get(nums[i]) == maxDegree){
-                minSubArrayLength = Math.min(minSubArrayLength, 
-                                              numsList.lastIndexOf(nums[i]) - numsList.indexOf(nums[i]) + 1);
-            }
+            if(leftMostIndexOfElementMap.get(nums[i]) == null)
+                leftMostIndexOfElementMap.put(nums[i], i);
+            rightMostIndexOfElementMap.put(nums[i], i);
+            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
         }
         
-        return minSubArrayLength == Integer.MAX_VALUE ? 0 : minSubArrayLength;
+        int minSubArrayLength = Integer.MAX_VALUE;
+        int maxDegree = Collections.max(count.values());
+        for(int ele : count.keySet()){
+            if(count.get(ele) == maxDegree)
+                minSubArrayLength = Math.min(minSubArrayLength,
+                                             rightMostIndexOfElementMap.get(ele) - leftMostIndexOfElementMap.get(ele) + 1);
+        }
         
+        return minSubArrayLength;
     }
+    
 }
