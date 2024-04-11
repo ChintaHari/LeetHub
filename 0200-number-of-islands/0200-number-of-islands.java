9a1204f5a65eb0d96417754eb522f10c6409ef69
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-04-10 21:34:41 -0700

    Time: 4 ms (49.20%), Space: 48.3 MB (99.29%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,55 +1,38 @@
 class Solution {
-    int m, n;
-    boolean visited[][];
-    int dirs[][] = new int[][]{{1,0}, {-1,0}, {0,1}, {0,-1}};
+    boolean[][] visited;
+    char[][] grid;
+    int numberOfIslands = 0;
+    int[][] dirs = new int[][]{{0,1}, {1,0}, {-1,0}, {0,-1}};
+    int rows = 0, cols = 0;
     public int numIslands(char[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
-        visited = new boolean[m][n];
-        int islands = 0;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] == '1' && !visited[i][j]){
-                    islands++;
-                    dfs(i, j, grid);
+        rows = grid.length;
+        cols = grid[0].length;
+        
+        visited = new boolean[rows][cols];
+        this.grid = grid;
+        
+        for(int row = 0; row < rows; row++){
+            for(int col=0; col < cols; col++){
+                if(grid[row][col] == '1' && !visited[row][col]){
+                    numberOfIslands++;
+                    dfs(row, col);
                 }
             }
         }
-        return islands;
-    }
-
-    public void dfs(int i, int j, char[][] grid){
-        visited[i][j] = true;
-        for(int[] dir: dirs){
-            int nextRow = i + dir[0];
-            int nextCol = j + dir[1];
-
-            if(nextRow >=0 && nextRow < m && nextCol >=0 && nextCol < n
-             && grid[nextRow][nextCol] == '1' && !visited[nextRow][nextCol]){
-                 //visited[nextRow][nextCol] = true;
-                 dfs(nextRow, nextCol, grid); 
-             }
-        }
-         
-//         Stack<Integer> rowStack = new Stack<>();
-//         Stack<Integer> colStack = new Stack<>();
-//         rowStack.push(i);   colStack.push(j);
-//         visited[i][j] = true;
-//         while(!rowStack.isEmpty()){
-//             int currRow = rowStack.pop();
-//             int currCol = colStack.pop();
-            
-//             for(int[] dir: dirs){
-//                 int nextRow = currRow + dir[0];
-//                 int nextCol = currCol + dir[1];
-//                 if(nextRow >=0 && nextRow < m && nextCol >=0 && nextCol < n
-//                  && grid[nextRow][nextCol] == '1' && !visited[nextRow][nextCol]){
-//                     visited[nextRow][nextCol] = true;
-//                     rowStack.push(nextRow);
-//                     colStack.push(nextCol);
-//                 }
-//             }
-//         }
         
+        return numberOfIslands;
+    }
+    
+    public void dfs(int row, int col){
+        visited[row][col] = true;
+        for(int[] dir : dirs){
+            int nextRow = row + dir[0];
+            int nextCol = col + dir[1];
+            
+            if(nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols
+               && !visited[nextRow][nextCol] && grid[nextRow][nextCol] == '1'){
+                dfs(nextRow, nextCol);
+            }
+        }
     }
 }
