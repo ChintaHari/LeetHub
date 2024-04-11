4a3a988b350455d0d621189a8bda215c2bdf09d2
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-04-11 14:39:04 -0700

    Time: 8 ms (88.34%), Space: 46.5 MB (49.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> result = new ArrayList<>();
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        List<int[]> result = new ArrayList<>();
         int[] currentInterval = intervals[0];
         result.add(currentInterval);
         
         for(int i=1; i<intervals.length; i++){
-            if(intervals[i][0] >  currentInterval[1]){
+            if(intervals[i][0] > currentInterval[1]){
                 currentInterval = intervals[i];
                 result.add(currentInterval);
             }
@@ -16,7 +16,6 @@ class Solution {
                 currentInterval[1] = Math.max(currentInterval[1], intervals[i][1]);
             }
         }
-        
         return result.toArray(new int[result.size()][]);
     }
 }
