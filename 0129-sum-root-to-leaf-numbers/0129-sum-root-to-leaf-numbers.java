dcfe0766d97cabb951b95b1f4afb655a01ca5422
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-04-14 17:58:03 -0700

    Time: 0 ms (100.00%), Space: 41.5 MB (17.41%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -13,22 +13,63 @@
  *     }
  * }
  */
-class Solution {
-    public int sumNumbers(TreeNode root) {
-        if(root.left == null && root.right == null)
-            return root.val;
+// class Solution {
+//     public int sumNumbers(TreeNode root) {
+// }
+//         if(root.left == null && root.right == null)
+//             return root.val;
         
-        return dfs(root, 0); 
-    }
+//         return dfs(root, 0); 
+//     }
     
-    public int dfs(TreeNode node, int number){
-        if(node == null)
-            return 0;
+//     public int dfs(TreeNode node, int number){
+//         if(node == null)
+//             return 0;
         
-        number = (number * 10) + node.val;
-        if(node.left == null && node.right == null)
-            return number;
+//         number = (number * 10) + node.val;
+//         if(node.left == null && node.right == null)
+//             return number;
         
-        return dfs(node.left, number) + dfs(node.right, number);
+//         return dfs(node.left, number) + dfs(node.right, number);
+//     }
+// }
+
+
+class Pair{
+    TreeNode node;
+    int number;
+    
+    Pair(TreeNode node, int number){
+        this.node = node;
+        this.number = number;
+    }
+}
+class Solution{
+    public int sumNumbers(TreeNode root) {
+        
+        Queue<Pair> queue = new LinkedList<>();
+        int sum = 0;
+        queue.offer(new Pair(root, 0));
+        
+        while(!queue.isEmpty()){
+            Pair pair = queue.poll();
+            TreeNode node = pair.node;
+            int currentNumber = pair.number;
+            
+            currentNumber = currentNumber * 10 + node.val;
+            
+            if(node.left == null && node.right == null)
+                sum = sum + currentNumber;
+            
+            else{
+                if(node.left != null)
+                    queue.offer(new Pair(node.left, currentNumber));
+                    
+                if(node.right != null)
+                    queue.offer(new Pair(node.right, currentNumber));
+            }
+        }
+        
+        return sum;
     }
 }
