d37e929a0531459e7d4506fafa08222f70e32acb
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-04-18 20:13:11 -0700

    Time: 4 ms (49.39%), Space: 48.7 MB (92.32%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,9 +1,9 @@
 class Solution {
-    boolean[][] visited;
     char[][] grid;
-    int numberOfIslands = 0;
+    boolean[][] visited;
+    int rows, cols;
     int[][] dirs = new int[][]{{0,1}, {1,0}, {-1,0}, {0,-1}};
-    int rows = 0, cols = 0;
+    int islands = 0;
     public int numIslands(char[][] grid) {
         rows = grid.length;
         cols = grid[0].length;
@@ -14,25 +14,24 @@ class Solution {
         for(int row = 0; row < rows; row++){
             for(int col=0; col < cols; col++){
                 if(grid[row][col] == '1' && !visited[row][col]){
-                    numberOfIslands++;
+                    islands++;
                     dfs(row, col);
                 }
             }
         }
-        
-        return numberOfIslands;
+        return islands;
     }
     
     public void dfs(int row, int col){
         visited[row][col] = true;
+        
         for(int[] dir : dirs){
             int nextRow = row + dir[0];
             int nextCol = col + dir[1];
             
-            if(nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols
-               && !visited[nextRow][nextCol] && grid[nextRow][nextCol] == '1'){
+            if(nextRow >= 0 && nextRow < rows && nextCol >=0 && nextCol < cols 
+               && !visited[nextRow][nextCol] && grid[nextRow][nextCol] == '1')
                 dfs(nextRow, nextCol);
-            }
         }
     }
 }
