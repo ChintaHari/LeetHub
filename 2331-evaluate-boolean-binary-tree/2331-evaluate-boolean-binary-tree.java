3207715ddca47683819946f73ace77d8c6e38585
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-05-15 18:25:49 -0700

    Time: 12 ms (100.00%), Space: 44.2 MB (53.53%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
@@ -13,7 +13,9 @@
  *     }
  * }
  */
-class Solution {
+
+// Recursive DFS
+/* class Solution {
     public boolean evaluateTree(TreeNode root) {
         if(root.left == null && root.right == null)
             return root.val == 1;
@@ -25,5 +27,35 @@ class Solution {
             return left | right;
         else
             return left & right;
+    }
+} */
+
+// Iterative DFS
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        Map<TreeNode, Boolean> map = new HashMap<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            TreeNode node = stack.peek();
+            if(node.left == null && node.right == null){
+                stack.pop();
+                map.put(node, node.val == 1);
+                continue;
+            }
+            if(map.containsKey(node.left) && map.containsKey(node.right)){
+                stack.pop();
+                if(node.val == 2)
+                    map.put(node, map.get(node.left) | map.get(node.right));
+                else
+                    map.put(node, map.get(node.left) & map.get(node.right));
+            }
+            else{
+                stack.push(node.left);
+                stack.push(node.right);
+            }
+        }
+        return map.get(root);
     }
 }
