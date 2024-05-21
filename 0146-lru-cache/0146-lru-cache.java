afce558afae2bdc4d7e340b6c4e3b824eff0c58c
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-05-21 13:52:49 -0700

    Time: 995 ms (5.04%), Space: 122.6 MB (25.68%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,38 +1,38 @@
 class LRUCache {
-    Map<Integer, Integer> cache;
+    HashMap<Integer, Integer> hm;
     LinkedList<Integer> list;
     int capacity;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        cache = new HashMap<>();
         list = new LinkedList<>();
+        hm = new HashMap<>();
     }
     
     public int get(int key) {
-        if(!cache.containsKey(key))
+        if(!hm.containsKey(key))
             return -1;
         
         list.remove(Integer.valueOf(key));
         list.addLast(key);
-        return cache.get(key);
+        return hm.get(key);
     }
     
     public void put(int key, int value) {
-        if(cache.containsKey(key)){
-            cache.put(key, value);
-            list.remove(Integer.valueOf(key));
-            list.addLast(key);
+        if(hm.containsKey(key)){
+            hm.put(key, value);
+             list.remove(Integer.valueOf(key));
+             list.addLast(key);
         }
         else{
             if(list.size() == capacity){
                 int removeFirst = list.removeFirst();
-                cache.remove(removeFirst);
+                hm.remove(removeFirst);
             }
-            
-            cache.put(key, value);
             list.addLast(key);
+            hm.put(key, value);
         }
+        
     }
 }
 
