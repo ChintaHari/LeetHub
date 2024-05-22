5749d17e4c053c468714a05951e2caf3e912bc14
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-05-22 14:24:06 -0700

    Time: 14 ms (87.43%), Space: 44.1 MB (76.53%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     int[] memo, coins;
     public int coinChange(int[] coins, int amount) {
         this.coins = coins;
@@ -29,3 +29,24 @@ class Solution {
         return memo[remain];
     }
 }
+*/
+
+//bottom-up approach.
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] ==  amount + 1 ? -1 : dp[amount];
+    }
+}
+
