852c2efe5542e1697ca3a38447585020daafd74c
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-05-27 22:34:03 -0500

    Time: 7 ms (73.45%), Space: 42.8 MB (77.64%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -1,18 +1,16 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
-        int max = Integer.MIN_VALUE;
-        int curr = 0;
         int left = 0, right = 0;
+        int maxLength = Integer.MIN_VALUE;
+        int currCost = 0;
         for(right = 0; right < s.length(); right++){
-            curr = curr + Math.abs(s.charAt(right) - t.charAt(right));
-            while(curr > maxCost){
-                curr = curr - Math.abs(s.charAt(left) - t.charAt(left));
-                left ++;
+            currCost = currCost + Math.abs(s.charAt(right) - t.charAt(right));
+            while(currCost > maxCost){
+                currCost = currCost - Math.abs(s.charAt(left) - t.charAt(left));
+                left++;
             }
-            
-            max = Math.max(max, right - left + 1);
+            maxLength = Math.max(maxLength, right - left + 1);
         }
-        return max == Integer.MIN_VALUE ? 0 : max;
-        
+        return maxLength == Integer.MIN_VALUE ? 0 : maxLength;
     }
 }
