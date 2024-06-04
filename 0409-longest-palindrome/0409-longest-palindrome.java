07eae1c78229735e08c712594217c0e52f3b3116
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-06-03 19:53:54 -0500

    Time: 9 ms (24.25%), Space: 42.2 MB (12.05%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,29 +1,31 @@
 class Solution {
     public int longestPalindrome(String s) {
-         Map<Character, Integer> map = new HashMap<>();
-        for(char ch: s.toCharArray())
-            map.put(ch, map.getOrDefault(ch,0)+1);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray())
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        
+        PriorityQueue<Character> heap = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+        for(char ch : map.keySet())
+            heap.offer(ch);
+        
         int maxLength = 0;
-        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
-        for(char ch: map.keySet())
-            maxHeap.add(ch);
-        while(!maxHeap.isEmpty()){
-            char pop = maxHeap.remove();
-            if(map.get(pop) == 1){
+        while(!heap.isEmpty()){
+            char ch = heap.remove();
+            
+            if(map.get(ch) == 1)
                 return maxLength + 1;
+            
+            if(map.get(ch) % 2 == 0){
+                maxLength = maxLength + map.get(ch);
+                map.put(ch, 0);
             }
-            if(map.get(pop)%2==0){
-                maxLength = maxLength + map.get(pop);
-                map.put(pop, 0);
+            
+            if(map.get(ch) % 2 != 0){
+                maxLength = maxLength + map.get(ch) - 1;
+                map.put(ch, 1);
+                heap.offer(ch);
             }
-            if(map.get(pop) %2 !=0){
-                maxLength = maxLength + map.get(pop)-1;
-                map.put(pop, 1);
-                maxHeap.add(pop);
-            }
-                
         }
         return maxLength;
-        
     }
 }
