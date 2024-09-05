0444accfcc2f19c4b291354c9d069e50dc13967d
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-09-05 18:10:30 -0400

    Time: 238 ms (21.18%), Space: 65.1 MB (98.44%) - LeetHub

--- 1882-process-tasks-using-servers/1882-process-tasks-using-servers.java
+++ 1882-process-tasks-using-servers/1882-process-tasks-using-servers.java
@@ -1,105 +1,40 @@
-class Available implements Comparable<Available> {
-    int weight;
-    int index;
-
-    public Available(int weight, int index) {
-        this.weight = weight;
-        this.index = index;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    @Override
-    public int compareTo(Available other) {
-        if (this.weight != other.weight) {
-            return this.weight - other.weight;
-        } else {
-            return this.index - other.index;
-        }
-    }
-}
-
-class Unavailable implements Comparable<Unavailable> {
-    int timeServerBecomesFree;
-    int weight;
-    int index;
-
-    public Unavailable(int timeServerBecomesFree, int weight, int index) {
-        this.timeServerBecomesFree = timeServerBecomesFree;
-        this.weight = weight;
-        this.index = index;
-    }
-
-    public int getTimeServerBecomesFree() {
-        return timeServerBecomesFree;
-    }
-
-    public void setTimeServerBecomesFree(int timeServerBecomesFree) {
-        this.timeServerBecomesFree = timeServerBecomesFree;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    @Override
-    public int compareTo(Unavailable other) {
-        return this.timeServerBecomesFree - other.timeServerBecomesFree;
-    }
-}
+import java.util.PriorityQueue;
 
 public class Solution {
     public int[] assignTasks(int[] servers, int[] tasks) {
-        int result[] = new int[tasks.length];
+        int[] result = new int[tasks.length];
 
-        PriorityQueue<Available> availableServers = new PriorityQueue<>();
-        PriorityQueue<Unavailable> unAvailableServers = new PriorityQueue<>();
+        PriorityQueue<int[]> availableServers = new PriorityQueue<>(
+            (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
 
-        for (int i = 0; i < servers.length; i++)
-            availableServers.offer(new Available(servers[i], i));
+        PriorityQueue<int[]> unAvailableServers = new PriorityQueue<>(
+            (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] != b[1] ? a[1] - b[1] : a[2] - b[2]);
+
+        for (int i = 0; i < servers.length; i++) 
+            availableServers.offer(new int[] {servers[i], i});
+        
 
         int time = 0;
         for (int i = 0; i < tasks.length; i++) {
             time = Math.max(time, i);
 
-            if (availableServers.isEmpty())
-                time = unAvailableServers.peek().getTimeServerBecomesFree();
-
-            while (!unAvailableServers.isEmpty() 
-                    && time >= unAvailableServers.peek().getTimeServerBecomesFree()) {
-                Unavailable unavailable = unAvailableServers.poll();
-                availableServers.offer(new Available(unavailable.getWeight(), unavailable.getIndex()));
+            while (!unAvailableServers.isEmpty() && unAvailableServers.peek()[0] <= time) {
+                int[] server = unAvailableServers.poll();
+                availableServers.offer(new int[] {server[1], server[2]});
             }
 
-            Available available = availableServers.poll();
-            result[i] = available.getIndex();
-            unAvailableServers.offer(new Unavailable(time + tasks[i], available.getWeight(), available.getIndex()));
+            if (availableServers.isEmpty()) {
+                time = unAvailableServers.peek()[0];
+                while (!unAvailableServers.isEmpty() && unAvailableServers.peek()[0] <= time) {
+                    int[] server = unAvailableServers.poll();
+                    availableServers.offer(new int[] {server[1], server[2]});
+                }
+            }
+
+            int[] server = availableServers.poll();
+            result[i] = server[1];
+
+            unAvailableServers.offer(new int[] {time + tasks[i], server[0], server[1]});
         }
 
         return result;
