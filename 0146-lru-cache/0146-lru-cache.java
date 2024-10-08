8056f669389769f97a20c56b3e6f08e8c0f38a94
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-10-08 15:38:14 -0500

    Time: 48 ms (52.35%), Space: 112.9 MB (71.72%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,37 +1,82 @@
 class LRUCache {
-    int capacity;
-    HashMap<Integer, Integer> cache;
-    List<Integer> list ;
 
+    class Node{
+        int key, value;
+        Node next, prev;
+        Node(int key, int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    Map<Integer, Node> map;
+    Node head, tail;
+    int capacity;
+    
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        cache = new HashMap<>();
-        list = new LinkedList<>();
+        map = new HashMap<>();
+        
+        head = new Node(0,0);
+        tail = new Node(0,0);
+        
+        head.next = tail;
+        tail.prev = head;
+        
+    }
+    
+    public void moveToHead(Node node){
+        removeNode(node);
+        addToFront(node);
+    }
+    
+    public void removeNode(Node node){
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+    
+    public void addToFront(Node node){
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+    
+    public Node removeTail(){
+        Node node = tail.prev;
+        removeNode(node);
+        return node;
     }
     
     public int get(int key) {
-        if(!cache.containsKey(key))
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            moveToHead(node);
+            return node.value;
+        }
+        else
             return -1;
-        
-        list.remove(Integer.valueOf(key));
-        list.addLast(key);
-        return cache.get(key);
     }
     
     public void put(int key, int value) {
-        if(cache.containsKey(key)){
-            list.remove(Integer.valueOf(key));
-            list.addLast(key);
-            cache.put(key, value);
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            node.value = value;
+            moveToHead(node);
         }
         else{
-            if(list.size() == capacity){
-                int removeFirst = list.removeFirst();
-                cache.remove(removeFirst);
+            Node newNode = new Node(key, value);
+            map.put(key, newNode);
+            addToFront(newNode);
+            
+            if(map.size() > capacity){
+                Node node = removeTail();
+                map.remove(node.key);
             }
-             list.addLast(key);
-            cache.put(key, value);
         }
+        
     }
 }
 
