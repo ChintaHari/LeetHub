abc3d2cab9cfcd1a4e3cf190d886fb51347ea82a
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2025-01-09 18:46:52 -0700

    Time: 13 ms, Memory: 54.9 MB - LeetHub

---
+++ 2757-240-916-word-subsets/2757-240-916-word-subsets.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        int[] maxFrequency = new int[26];
+        
+        for (String subset : words2) {
+            int[] frequency = new int[26];
+            for (char c : subset.toCharArray()) 
+                frequency[c - 'a']++;
+            
+            for (int i = 0; i < 26; i++) 
+                maxFrequency[i] = Math.max(maxFrequency[i], frequency[i]);
+        }
+        
+        List<String> universalStrings = new ArrayList<>();
+        for (String word : words1) {
+            int[] frequency = new int[26];
+            for (char c : word.toCharArray()) 
+                frequency[c - 'a']++;
+            
+            boolean isUniversal = true;
+            for (int i = 0; i < 26; i++) {
+                if (frequency[i] < maxFrequency[i]) {
+                    isUniversal = false;
+                    break;
+                }
+            }
+            
+            if (isUniversal)
+                universalStrings.add(word);
+        }
+        return universalStrings;
+    }
+}
+
