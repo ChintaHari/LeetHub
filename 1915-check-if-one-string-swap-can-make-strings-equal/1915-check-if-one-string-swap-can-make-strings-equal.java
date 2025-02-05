21cd1cb0bf0c3d5e23a59b94c07ee956222214bb
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2025-02-04 18:40:42 -0700

    Time: 1 ms (52.21%), Space: 41.5 MB (76.92%) - LeetHub

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        List<Integer> differentIndices = new ArrayList<>();
+        for(int i=0; i<s1.length(); i++){
+            if(s1.charAt(i) != s2.charAt(i))
+                differentIndices.add(i);
+        }
+        
+        if(differentIndices.size() == 0)
+            return true;
+        else if (differentIndices.size() == 2){
+            if(s1.charAt(differentIndices.get(0)) == s2.charAt(differentIndices.get(1)) &&
+              s1.charAt(differentIndices.get(1)) == s2.charAt(differentIndices.get(0)))
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+        
+    }
+}
